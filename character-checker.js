28f740d03866da968cc77eadf320ac166bd99cd3
KalynnReshelle <kalynnreshellee@gmail.com>
2025-09-07 21:48:10 -0500

    Initial commit

--- character-checker.js
+++ character-checker.js
@@ -1,1 +1,6 @@
+const readline= require('readline-sync');
 
+let favoriteColor = readline.question("Enter your favorite color: ");
+let selectedCharacter= readline.questionInt(" Choose any random character: ");
+
+console.log(" You chose character: "+ favoriteColor[selectedCharacter]);
